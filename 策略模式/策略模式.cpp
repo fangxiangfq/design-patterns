d2ffc6c03b4fc6addd270e8fae84b2f6b4048801
fangxiang <1627102210@qq.com>
2022-02-13 00:04:11 +0800

    策略模式

---
+++ 策略模式/策略模式.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <memory>
+
+class Strategy {
+public:
+	virtual void algorithm() = 0;
+};
+
+class StrategyA : public Strategy {
+public:
+	void algorithm() override {
+		std::cout << "StrategyA" << std::endl;
+	}
+};
+
+class StrategyB : public Strategy {
+public:
+	void algorithm() override {
+		std::cout << "StrategyB" << std::endl;
+	}
+};
+
+class StrategyC : public Strategy {
+public:
+	void algorithm() override {
+		std::cout << "StrategyC" << std::endl;
+	}
+};
+
+class Context {
+public:
+	Context(Strategy* s) : strategy_(s) {
+
+	}
+
+	void method() {
+		strategy_->algorithm();
+	}
+
+private:
+	std::unique_ptr<Strategy> strategy_;
+};
+
+int main() {
+	std::unique_ptr<Context> cx = std::make_unique<Context>(new StrategyA());
+	cx->method();
+	cx = std::make_unique<Context>(new StrategyB());
+	cx->method();
+	cx = std::make_unique<Context>(new StrategyC());
+	cx->method();
+	system("pause");
+
+	return 0;
+}
+
+
--- 简单工厂模式/简单工厂模式.cpp
+++ 简单工厂模式/简单工厂模式.cpp
@@ -1,20 +1,92 @@
-﻿// 简单工厂模式.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <exception>
+#include <memory>
+#include <string>
 
-#include <iostream>
+class Operation {
+protected:
+	double lhs_{ 0.0 };
+	double rhs_{ 0.0 };
 
-int main()
-{
-    std::cout << "Hello World!\n";
+public:
+	void set(double lhs, double rhs) {
+		lhs_ = lhs;
+		rhs_ = rhs;
+	}
+
+	virtual double result() {
+		return 0.0;
+	}
+
+	virtual ~Operation() {}
+};
+
+class OperationAdd : public Operation {
+public:
+	double result() override {
+		return lhs_ + rhs_;
+	}
+};
+
+class OperationSub : public Operation {
+public:
+	double result() override {
+		return lhs_ - rhs_;
+	}
+};
+
+class OperationMix : public Operation {
+public:
+	double result() override {
+		return lhs_ * rhs_;
+	}
+};
+
+class OperationDiv : public Operation {
+public:
+	double result() override {
+		if (0 == rhs_) {
+			throw std::out_of_range("divide zero!");
+		}
+		return lhs_ / rhs_;
+	}
+};
+
+class OperationFactory final {
+public:
+	static std::unique_ptr<Operation> operate(const std::string& op) noexcept {
+		if ("+" == op) {
+			return std::make_unique<OperationAdd>();
+		}
+		else if ("-" == op) {
+			return std::make_unique<OperationSub>();
+		}
+		else if ("*" == op) {
+			return std::make_unique<OperationMix>();
+		}
+		else if ("/" == op) {
+			return std::make_unique<OperationDiv>();
+		}
+
+		return nullptr;
+	}
+};
+
+int main() {
+    auto op = OperationFactory::operate("+");
+	if (op) {
+		op->set(12.8, 11.4);
+		std::cout << op->result() << std::endl;
+	}
+	op = OperationFactory::operate("/");
+	op->set(12.8, 0);
+	try {
+		std::cout << op->result() << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	system("pause");
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件

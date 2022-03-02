e7dc643b0b84f3f527d3d20ed156b898a16d7539
fangxiang <1627102210@qq.com>
2022-03-02 22:35:33 +0800

    建造模式

---
+++ 建造模式/建造模式.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+#include <string>
+
+using std::list;
+using std::string;
+
+class Product {
+public:
+	void add(const string& s) {
+		parts_.push_back(s);
+	}
+
+	void show() {
+		for_each(parts_.begin(), parts_.end(), [](const string& s) { std::cout << s << " "; });
+		std::cout << std::endl;
+	}
+
+private:
+	list<string> parts_;
+};
+
+class Builder {
+public:
+	virtual void buildA() = 0;
+	virtual void buildB() = 0;
+	virtual Product* result() { return p_; }
+
+	Builder() :p_(new Product()) {}
+	virtual ~Builder() = default;
+
+protected:
+	Product* p_;
+};
+
+class ConcreteBuilderA : public Builder {
+public:
+	virtual void buildA() override {
+		p_->add("v");
+	};
+	virtual void buildB() override {
+		p_->add("w");
+	};
+};
+
+class ConcreteBuilderB : public Builder {
+public:
+	virtual void buildA() override {
+		p_->add("x");
+	};
+	virtual void buildB() override {
+		p_->add("y");
+	};
+};
+
+class Director {
+public:
+	void construct(Builder& builder) {
+		builder.buildA();
+		builder.buildB();
+	}
+};
+
+int main()
+{
+	Director d;
+	ConcreteBuilderA a;
+	ConcreteBuilderB b;
+
+	d.construct(a);
+	d.construct(b);
+
+	a.result()->show();
+	b.result()->show();
+
+	system("pause");
+
+	return 0;
+}
+

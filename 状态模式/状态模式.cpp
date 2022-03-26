cf6e4c1732d8f40871b6348f241d78626d2ec177
fangxiang <1627102210@qq.com>
2022-03-26 20:08:58 +0800

    适配器模式

---
+++ 状态模式/状态模式.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <memory>
+
+class Context;
+
+class State {
+public:
+	virtual void handle(Context& text) = 0;
+};
+
+class Context {
+public:
+	Context(std::shared_ptr<State> state) {
+		state_ = state;
+	}
+
+	void request() {
+		state_->handle(*this);
+	}
+
+	void set(std::shared_ptr<State> state) {
+		state_ = state;
+	}
+private:
+	std::shared_ptr<State> state_;
+};
+
+class StateB;
+class StateA : public State {
+	void handle(Context& text) {
+		text.set(std::shared_ptr<State>(new StateB()));
+	}
+};
+
+class StateB : public State {
+	void handle(Context& text) {
+		text.set(std::shared_ptr<State>(new StateA()));
+	}
+};
+
+int main()
+{
+    
+}
+
+
---
+++ 适配器模式/适配器模式.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+class Target {
+public:
+	virtual void request() {
+		std::cout << "普通请求" << std::endl;
+	}
+};
+
+class Adaptee {
+public:
+	void speicalRequest() {
+		std::cout << "特殊请求" << std::endl;
+	}
+};
+
+class Adapter : public Target {
+public:
+	void request() override {
+		adaptee_.speicalRequest();
+	}
+private:
+	Adaptee adaptee_;
+};
+
+int main()
+{
+	Adapter adapter;
+	Target& t = adapter;
+	t.request();
+}
+

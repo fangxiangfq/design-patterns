154730e51ba3db5943528284c9b957fc6dcebe2e
fangxiang <1627102210@qq.com>
2022-04-10 16:17:09 +0800

    单例桥接

---
+++ 单例模式/单例模式.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+class Singleton {
+public:
+	static Singleton* GetInstance() {
+		if (!singleton_) {
+			singleton_ = new Singleton();
+		}
+
+		return singleton_;
+	}
+
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+
+private:
+	Singleton() = default;
+	static Singleton* singleton_;
+	struct Gc {
+		~Gc() {
+			if (singleton_) {
+				delete singleton_;
+				singleton_ = nullptr;
+			}
+		}
+	};
+	static Gc gc_;
+};
+
+Singleton* Singleton::singleton_;
+
+int main()
+{
+	auto s = Singleton::GetInstance();
+    std::cout << "Hello World!\n";
+	return 0;
+}
---
+++ 桥接模式/桥接模式.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+class Implementor {
+public:
+	virtual void operation() = 0;
+};
+
+class ConcreteImplementorA : public Implementor {
+public:
+	void operation() override {
+		std::cout << "ConcreteImplementorA\n";
+	}
+};
+
+class ConcreteImplementorB : public Implementor {
+	void operation() override {
+		std::cout << "ConcreteImplementorB\n";
+	}
+};
+
+class Abstraction {
+public:
+	virtual void operation() {
+		std::cout << "Abstraction\n";
+		if (impl_) {
+			impl_->operation();
+		}
+	}
+
+	virtual void set(Implementor* impl) {
+		impl_ = impl;
+	}
+
+
+	virtual ~Abstraction() = 0 {}
+protected:
+	Implementor* impl_;
+};
+
+class RefinedAbstraction : public Abstraction {
+public:
+	void operation() override {
+		std::cout << "RefinedAbstraction\n";
+		if (impl_) {
+			impl_->operation();
+		}
+	}
+};
+
+int main()
+{
+	RefinedAbstraction r;
+	Abstraction& a = r;
+	ConcreteImplementorA ia;
+	ConcreteImplementorA ib;
+	a.set(&ia);
+	a.operation();
+
+	return 0;
+}
+
